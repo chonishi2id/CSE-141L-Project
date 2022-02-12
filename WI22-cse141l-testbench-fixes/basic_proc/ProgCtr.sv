8e3bcb46f8b604ad856df8f9b402d5896ce4f696
Matthew Larkins <larkins.07@gmail.com>
2022-02-12 00:46:44 -0800

    finish merge

--- WI22-cse141l-testbench-fixes/basic_proc/ProgCtr.sv
+++ WI22-cse141l-testbench-fixes/basic_proc/ProgCtr.sv
@@ -9,11 +9,7 @@ module ProgCtr #(parameter L=10) (
   input                Reset,      // reset, init, etc. -- force PC to 0
                        Start,      // Signal to jump to next program; currently unused 
                        Clk,        // PC can change on pos. edges only
-<<<<<<< HEAD
-					   BranchAbs,  // jump to Target
-=======
 					   BranchAbsEn,  // jump to Target
->>>>>>> 788e6f0d53329dbc62561cb187fb64807a354675
 					   ALU_flag,   // Sometimes you may require signals from other modules, can pass around flags if needed
   input        [L-1:0] Target,     // jump ... "how high?"
   output logic [L-1:0] ProgCtr     // the program counter register itself
@@ -23,15 +19,9 @@ module ProgCtr #(parameter L=10) (
   // program counter can clear to 0, increment, or jump
   always_ff @(posedge Clk)         // or just always; always_ff is a linting construct
 	if(Reset)
-<<<<<<< HEAD
-	  ProgCtr <= 0;				       // for first program; want different value for 2nd or 3rd
-	else if(BranchAbs)	               // unconditional absolute jump (branch if equal)
-	  ProgCtr <= Target;			   // how would you make it unconditional and/or absolute
-=======
 	  ProgCtr <= 0;	
 	else if(BranchAbsEn)	               // unconditional absolute jump
 	  ProgCtr <= Target;			   //   how would you make it conditional and/or relative?
->>>>>>> 788e6f0d53329dbc62561cb187fb64807a354675
 	else
 	  ProgCtr <= ProgCtr+'b1; 	       // default increment (no need for ARM/MIPS +4 -- why?)
 endmodule
