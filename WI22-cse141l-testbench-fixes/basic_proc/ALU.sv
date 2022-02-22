f01f3826e00f65d9df824dee595984bdb1eafc89
Johnny <jangiuli@ucsd.edu>
2022-02-21 21:11:18 -0800

    Merge branch 'main' of https://github.com/ecstaticstupidity/CSE-141L-Project

--- WI22-cse141l-testbench-fixes/basic_proc/ALU.sv
+++ WI22-cse141l-testbench-fixes/basic_proc/ALU.sv
@@ -11,13 +11,14 @@ module ALU #(parameter W=8, Ops=4)(
                          InputB,
   input        [Ops-1:0] OP,		      // ALU opcode, part of microcode
   output logic [W-1:0]   Out,		      // data output 
-  output logic           Zero         // output = zero flag	 !(Out)
+  output logic           Branch         // output = zero flag	 !(Out)
   );								    
 	 
   op_mne op_mnemonic;			          // type enum: used for convenient waveform viewing
 	
   always_comb begin
-    Out = 0;                              // No Op = default
+    Out = 0; // No Op = default
+    Branch = 0;                             
     case (OP)							  
       ADD : Out = InputA + InputB;        // add 
       LSH : Out = {InputA[6:0], 1'b0};    // shift left, fill in with zeroes 
@@ -28,12 +29,9 @@ module ALU #(parameter W=8, Ops=4)(
       GEQ : Out = (InputA >= InputB);         // Greater than or Equal to
       EQ  : Out = (InputA == InputB);        // Equals to
       NEQ : Out = (InputA != InputB);         // Not Equals to
+      BNZ : Branch = (InputA != 0);      //Branch not equal to zero
     endcase
   end
-
-  assign Zero   = !Out;                   // reduction NOR
-  assign Parity = ^Out;                   // reduction XOR
-  assign Odd    = Out[0];				  // odd/even -- just the value of the LSB
 
   always_comb
     op_mnemonic = op_mne'(OP);			  // displays operation name in waveform viewer
