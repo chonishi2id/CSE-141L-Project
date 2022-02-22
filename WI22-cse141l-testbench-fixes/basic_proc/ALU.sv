b6e1a13cb90fb9071acf05db5ec93a9ed013a787
Matthew Larkins <larkins.07@gmail.com>
2022-02-21 21:06:32 -0800

    update ALU

--- WI22-cse141l-testbench-fixes/basic_proc/ALU.sv
+++ WI22-cse141l-testbench-fixes/basic_proc/ALU.sv
@@ -17,7 +17,9 @@ module ALU #(parameter W=8, Ops=4)(
   op_mne op_mnemonic;			          // type enum: used for convenient waveform viewing
 	
   always_comb begin
-    Out = 0;                              // No Op = default
+    Out = 0;      
+    Branch = 0;
+                        // No Op = default
     case (OP)							  
       ADD : Out = InputA + InputB;        // add 
       LSH : Out = {InputA[6:0], 1'b0};    // shift left, fill in with zeroes 
@@ -28,14 +30,10 @@ module ALU #(parameter W=8, Ops=4)(
       GEQ : Out = (InputA >= InputB);         // Greater than or Equal to
       EQ  : Out = (InputA == InputB);        // Equals to
       NEQ : Out = (InputA != InputB);         // Not Equals to
+      BNZ : branch = (InputA != 0);
     endcase
   end
 
-  assign Zero   = !Out;                   // reduction NOR
-  assign Parity = ^Out;                   // reduction XOR
-  assign Odd    = Out[0];				  // odd/even -- just the value of the LSB
-
   always_comb
     op_mnemonic = op_mne'(OP);			  // displays operation name in waveform viewer
-
 endmodule
