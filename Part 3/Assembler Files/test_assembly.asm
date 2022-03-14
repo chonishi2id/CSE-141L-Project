951f65881319e4bd6d7f26fc8d47e29249062217
expjel <59629667+expjel@users.noreply.github.com>
2022-03-14 02:55:40 -0700

    updated assembly

--- Part 3/Assembler Files/test_assembly.asm
+++ Part 3/Assembler Files/test_assembly.asm
@@ -22,8 +22,8 @@ str r2, r0
 //equals: r1 = (r1 == r2)
 eq r1, r2 
 
-//branch if not equal to zero: if r1 == 1, go to address in R3
-bnz r3, r1
+//branch if not equal to zero: if r1 == 1, use offset stored in R3
+bnzr r3, r1
 
 //greater than or equal to: r0 = (r0 >= r1)
 geq r0, r1
@@ -39,3 +39,6 @@ addi r0, #3
 
 //not equal to: r0 = (r0 != r1)
 neq r0, r1
+
+//bnzl branch if not equal to zero: if r1 == 1, use value stored in R3 to index into LUT
+bnzl r3, r1
