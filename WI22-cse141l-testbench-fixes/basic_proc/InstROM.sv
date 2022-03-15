ecd2e17ecc892210d9972fdc83146fd144e1cdd5
Matthew Larkins <larkins.07@gmail.com>
2022-03-14 21:38:34 -0700

    cleanup code

--- WI22-cse141l-testbench-fixes/basic_proc/InstROM.sv
+++ WI22-cse141l-testbench-fixes/basic_proc/InstROM.sv
@@ -5,21 +5,15 @@ module InstROM (
   input       [9:0] InstAddress,    // 10-bit addressing for instructions in case we have need for that many instructions
   output logic[8:0] InstOut);       // 9-bit instructions per project specification
 	 
-// Second option - read instructions from file machine_code.txt
-// declare 2-dimensional array, W bits wide, 2**A words deep
+// read instructions from file machine_code.txt
+// declare 2-dimensional array, 10 bits wide, 2^10 words deep
   logic[8:0] inst_rom[2**10];
   always_comb InstOut = inst_rom[InstAddress];
 
   // Load instruction memory from external file
   initial begin
-  	// NOTE: This may not work depending on your simulator
-	//       e.g. Questa needs the file in path of the application .exe, it
-	//       doesn't care where you project code is
 	$readmemb("machine_out_prog1.hex", inst_rom);
-	
-	// So you are probably better off with an absolute path,
-	// but you will have to change this example path when you
-	// try this on your machine most likely:
+      // could also use absolute path 
 	//$readmemb("/Users/matthewalanlarkins/Desktop/machine_code.txt", inst_rom);
   end 
 endmodule
