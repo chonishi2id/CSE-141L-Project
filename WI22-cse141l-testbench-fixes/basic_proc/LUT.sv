7ce1bb423781335f4bf24a6a0e0a4a2107fa00e0
Matthew Larkins <larkins.07@gmail.com>
2022-03-16 06:03:03 -0700

    update offset location in LUT

--- WI22-cse141l-testbench-fixes/basic_proc/LUT.sv
+++ WI22-cse141l-testbench-fixes/basic_proc/LUT.sv
@@ -10,14 +10,14 @@ module LUT(
     always_comb begin
         // powers of 4
         case(Index)
-            0:  Out = -370;  
+            0:  Out = -370;  // (prog1 bnzl instruction)
             1:  Out = 1;  
             2:  Out = 1;  
             3:  Out = 1;  
             4:  Out = 1;  
-            5:  Out = 1; // (prog1 bnzl instruction)
+            5:  Out = 1; 
             6:  Out = 1;
-            7:  Out = 1;
+            7:  Out = -357; // (prog3 bnzl instruction)
             8:  Out = 1;  
             9:  Out = 1;  
             10: Out = 1; 
@@ -25,7 +25,7 @@ module LUT(
             12: Out = 1; 
             13: Out = 1; 
             14: Out = 1; 
-            15: Out = -357;
+            15: Out = 1;
         endcase
     end
 endmodule
